c850eebefee68bc0ee24d7e74ddc8312890d73f7
ferizzon <rizzon.fil@gmail.com>
2026-06-05 15:25:47 -0300

    Sincronizando chaves JSON do PHP com o JS

--- enviar.php
+++ enviar.php
@@ -15,12 +15,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $para = "rizzon.fil@gmail.com";
-    $assunto = "Nova Solicitacao de Reparo - Consertoculos"; // Sem caracteres especiais ou emojis temporariamente
+    $assunto = "Nova Solicitacao de Reparo - Consertoculos";
     
     // Configuração do delimitador do e-mail com anexos
     $boundary = md5(time());
     
-    // Cabeçalhos do e-mail padronizados
+    // Cabeçalhos do e-mail padronizados para o Gmail aceitar
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "From: Consertoculos Lab <alante87@alanteixeiralopes1780672994593.2101853.meusitehostgator.com.br>\r\n";
     $headers .= "Reply-To: $email\r\n";
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Corpo da mensagem em HTML com quebras corrigidas para o Gmail
     $corpo = "--$boundary\r\n";
     $corpo .= "Content-Type: text/html; charset=UTF-8\r\n";
-    $corpo .= "Content-Transfer-Encoding: 8bit\r\n\r\n"; // Mudado para 8bit (melhor para acentos em HTML)
+    $corpo .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
     $corpo .= "<h2>Nova mensagem recebida pelo site:</h2>";
     $corpo .= "<p><strong>Nome:</strong> $nome</p>";
     $corpo .= "<p><strong>E-mail:</strong> $email</p>";
@@ -44,13 +44,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $fileSize = $_FILES['imagens']['size'][$index];
                 $fileType = $_FILES['imagens']['type'][$index];
                 
-                // Abre e lê o arquivo temporário
                 $file = fopen($tmpName, "rb");
                 $data = fread($file, $fileSize);
                 fclose($file);
                 $data = chunk_split(base64_encode($data));
                 
-                // Anexa o arquivo ao corpo do e-mail
                 $corpo .= "--$boundary\r\n";
                 $corpo .= "Content-Type: $fileType; name=\"$fileName\"\r\n";
                 $corpo .= "Content-Disposition: attachment; filename=\"$fileName\"\r\n";
@@ -65,9 +63,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Envia o e-mail usando o servidor nativo da HostGator
     if (mail($para, $assunto, $corpo, $headers)) {
         http_response_code(200);
+        // AJUSTADO: Chave alterada de "mensagem" para "message" para bater com seu JS line 194
         echo json_encode(["message" => "Solicitação enviada com sucesso! Nossa equipe analisará as imagens."]);
     } else {
         http_response_code(500);
+        // AJUSTADO: Chave alterada de "erro" para "error" para bater com seu JS line 197
         echo json_encode(["error" => "Ocorreu um erro interno ao enviar o e-mail. Tente novamente."]);
     }
 } else {
